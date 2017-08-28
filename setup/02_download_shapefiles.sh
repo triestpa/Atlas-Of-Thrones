c6a92a159bd60503d6b38fc36f5dcef6ebd031f3
triestpa <patrick.triest@gmail.com>
2017-08-28 20:20:30 +0700

    Switch setup scripts to use self-hosted shapefiles

--- setup/02_download_shapefiles.sh
+++ setup/02_download_shapefiles.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 mkdir data
 cd data
-wget https://cdn.patricktriest.com/shapefiles/GISofThrones.zip
-unzip -a GISofThrones.zip
+wget https://cdn.patricktriest.com/shapefiles/game_of_thrones_shapes.zip
+unzip -a game_of_thrones_shapes
--- setup/03_load_db.sh
+++ setup/03_load_db.sh
@@ -1,12 +1,12 @@
 #!bin/bash
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Continents.shp continents | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Islands.shp islands | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Lakes.shp lakes | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Land.shp land | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Landscape.shp landscape | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Locations.shp locations | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Political.shp political | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Regions.shp regions | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Rivers.shp rivers | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Roads.shp roads | psql -d got_map
-shp2pgsql -I -G -s 4326 ./data/GISofThrones/GoTRelease/Wall.shp wall | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Continents.shp continents | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Islands.shp islands | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Lakes.shp lakes | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Land.shp land | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Landscape.shp landscape | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Locations.shp locations | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Political.shp political | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Regions.shp regions | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Rivers.shp rivers | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Roads.shp roads | psql -d got_map
+shp2pgsql -I -G -s 4326 ./data/game_of_thrones_shapes/Wall.shp wall | psql -d got_map
