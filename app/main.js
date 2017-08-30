eb49beae0eb18f0d33420d166f7b031240da9bf4
triestpa <patrick.triest@gmail.com>
2017-08-30 17:55:15 +0700

    Simplify window view controller binding

--- app/main.js
+++ app/main.js
@@ -1,11 +1,3 @@
 import { ViewController } from './view'
 require('./styles.scss')
-
-const controller = new ViewController()
-controller.loadMapData()
-
-window.toggleInfo = controller.toggleInfo
-window.toggleLayer = (layerName) => {
-  console.log(layerName)
-  controller.toggleLayer(layerName)
-}
+window.controller = new ViewController()
