9ea6c20e19c5f8e470f93603d8f63d32d1d123cd
triestpa <patrick.triest@gmail.com>
2017-08-31 15:55:52 +0700

    Configure global logger module

---
+++ server/logger.js
@@ -0,0 +1,25 @@
+/**
+ * Winston Logger Module
+ */
+
+const winston = require('winston')
+const path = require('path')
+
+// Configure custom app-wide logger
+const logger = new winston.Logger({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({
+      name: 'info-file',
+      filename: path.resolve(__dirname, '../info.log'),
+      level: 'info'
+    }),
+    new (winston.transports.File)({
+      name: 'error-file',
+      filename: path.resolve(__dirname, '../error.log'),
+      level: 'error'
+    })
+  ]
+})
+
+module.exports = logger
