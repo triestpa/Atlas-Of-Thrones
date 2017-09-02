9d71565f354db327a04b502557d3ea2e9a9f31e1
triestpa <patrick.triest@gmail.com>
2017-09-02 17:25:59 +0700

    Add psql setup files

---
+++ psql_setup/load_db.sh
@@ -0,0 +1,3 @@
+psql -f setup_db.sql
+shp2pgsql -I -G -s 4326 shapefiles/kingdoms.shp kingdoms | psql -d atlas_of_thrones
+shp2pgsql -I -G -s 4326 shapefiles/locations.shp locations | psql -d atlas_of_thrones
