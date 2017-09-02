4c297d74523175b4a317e82d983c937b70674596
triestpa <patrick.triest@gmail.com>
2017-09-02 17:31:40 +0700

    Enable all layers by default

--- app/main.js
+++ app/main.js
@@ -36,6 +36,7 @@ export class ViewController {
       const geojson = await this.api.getLocations(locationType)
       this.locationSearch.addGeoJsonItems(geojson, locationType)
       this.mapController.addLocationGeojson(locationType, geojson, locationLayers[locationType])
+      this.toggleMapLayer(locationType)
     }
 
     // Download kingdom boundaries
