596be8ce17f1487155410aab7b1909d733d9d19e
triestpa <patrick.triest@gmail.com>
2017-09-02 19:24:05 +0700

    Reconfigure psql setup to use DB dump instead of shapefiles
    This is because the shapefiles were trimming the summary texts.

--- psql_setup/load_db.sh
+++ psql_setup/load_db.sh
@@ -1,4 +1,3 @@
 #!bin/bash
 psql -f db_init.sql
-shp2pgsql -I -G -s 4326 shapefiles/kingdoms.shp kingdoms | psql -d atlas_of_thrones
-shp2pgsql -I -G -s 4326 shapefiles/locations.shp locations | psql -d atlas_of_thrones
+psql atlas_of_thrones < atlas_of_thrones.sql
