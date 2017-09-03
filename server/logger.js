48c3847db2e9823afd6c3dfd98fd57c5351e4439
triestpa <patrick.triest@gmail.com>
2017-09-03 15:48:12 +0700

    Reduce code in template directory

--- server/logger.js
+++ server/logger.js
@@ -1,23 +1,3 @@
 /**
  * Winston Logger Module
  */
-
-const winston = require('winston')
-const path = require('path')
-
-// Configure custom app-wide logger
-module.exports = new winston.Logger({
-  transports: [
-    new (winston.transports.Console)(),
-    new (winston.transports.File)({
-      name: 'info-file',
-      filename: path.resolve(__dirname, '../info.log'),
-      level: 'info'
-    }),
-    new (winston.transports.File)({
-      name: 'error-file',
-      filename: path.resolve(__dirname, '../error.log'),
-      level: 'error'
-    })
-  ]
-})
