5c384d7f4c4f3e4ff2867cd0dadd1bd2a383e579
triestpa <patrick.triest@gmail.com>
2017-09-03 11:00:33 +0700

    Remove unwanted files for backend-starter template

--- psql_setup/db_init.sql
+++
@@ -1,9 +0,0 @@
--- Reset DB
-DROP DATABASE IF EXISTS atlas_of_thrones;
-CREATE DATABASE atlas_of_thrones;
-
--- Connect to DB
-\c atlas_of_thrones
-
--- Enable postgis
-CREATE EXTENSION postgis;
--- psql_setup/load_db.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-psql -f db_init.sql
-psql atlas_of_thrones < atlas_of_thrones.sql
