7041293b932c095d545577e8a4b8354460859799
triestpa <patrick.triest@gmail.com>
2017-09-05 11:37:32 +0700

    Replace node-redis with io-redis

--- server/cache.js
+++ server/cache.js
@@ -2,33 +2,6 @@
  * Redis Cache Module
  */
 
-const redis = require('redis')
-const log = require('./logger')
-const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST)
-
-// Catch and log Redis error
-client.on('error', function (err) {
-  log.error('Redis Error', err)
-})
-
-module.exports = {
-  /** Check the cache for the key */
-  get: (key) => {
-    return new Promise((resolve, reject) => {
-      client.get(key, function (err, data) {
-        if (err) { reject(err) }
-        resolve(data)
-      })
-    })
-  },
-
-  /** Cache the value with the key */
-  set: (key, value) => {
-    return new Promise((resolve, reject) => {
-      client.set(key, value, function (err, reply) {
-        if (err) { reject(err) }
-        resolve(reply)
-      })
-    })
-  }
-}
+const Redis = require('ioredis')
+const redis = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST)
+module.exports = redis
