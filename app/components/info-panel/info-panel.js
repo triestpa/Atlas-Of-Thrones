4daa53b7dd97d8970a112ddb8b9691c0e8abd6e7
triestpa <patrick.triest@gmail.com>
2017-09-06 18:59:22 +0700

    Move info panel into own component directory

---
+++ app/components/info-panel/info-panel.js
@@ -0,0 +1,72 @@
+import './info-panel.scss'
+import template from './info-panel.html'
+
+export class InfoPanelComponent {
+  constructor (placeholderId, api) {
+    document.getElementById(placeholderId).outerHTML = template
+    this.api = api
+  }
+
+  /** Show info when a map item is selected */
+  async showInfo (name, id, type) {
+    const infoTitle = document.getElementById('info-title')
+    infoTitle.innerHTML = `<h1>${name}</h1>`
+
+    const infoContent = document.getElementById('info-content')
+    if (id && type === 'kingdom') {
+      infoContent.innerHTML = await this.getKingdomDetailHtml(id)
+    } else {
+      infoContent.innerHTML = await this.getLocationDetailHtml(id, type)
+    }
+
+    // Show info window if hidden, and on desktop
+    const infoContainer = document.getElementById('info-container')
+    if (!infoContainer.classList.contains('info-active') && window.innerWidth > 600) {
+      this.toggleInfo()
+    }
+  }
+
+  /** Create kingdom detail HTML string */
+  async getKingdomDetailHtml (id) {
+    let size = await this.api.getRegionSize(id)
+    size = size.toLocaleString(undefined, { maximumFractionDigits: 0 })
+
+    const castleCount = await this.api.getCastleCount(id)
+    const kingdomInfo = await this.api.getRegionDetails(id)
+    const summaryHTML = this.getInfoSummaryHtml(kingdomInfo)
+
+    return `
+      <h3>KINGDOM</h3>
+      <div>Size Estimate - ${size} km<sup>2</sup></div>
+      <div>Number of Castles - ${castleCount}</div>
+      ${summaryHTML}
+      `
+  }
+
+  /** Create location detail HTML string */
+  async getLocationDetailHtml (id, type) {
+    const locationInfo = await this.api.getLocationDetails(id)
+    const summaryHTML = this.getInfoSummaryHtml(locationInfo)
+    return `
+      <h3>${type.toUpperCase()}</h3>
+      ${summaryHTML}
+      `
+  }
+
+  /** Format location summary HTML */
+  getInfoSummaryHtml (info) {
+    return `
+      <h3>Summary</h3>
+      <div>${info.summary}</div>
+      <div>
+        <a href="${info.url}" target="_blank" rel="noopener">Read More...</a>
+      </div>
+    `
+  }
+
+  /** Toggle the info container */
+  toggleInfo () {
+    const infoContainer = document.getElementById('info-container')
+    infoContainer.classList.toggle('info-active')
+  }
+}
--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ import { MapApi } from './api'
 import { LocationSearch } from './search'
 import { MapController } from './map'
 import { LayerPanelComponent } from './components/layer-panel/layer-panel'
+import { InfoPanelComponent } from './components/info-panel/info-panel'
 
 /** Main UI Controller Class */
 export class ViewController {
@@ -14,9 +15,9 @@ export class ViewController {
       this.api = new MapApi('https://api.atlasofthrones.com/')
     }
 
-    this.mapController = new MapController((name, id, type) => this.showInfo(name, id, type))
+    this.infoContainer = new InfoPanelComponent('info-panel-placeholder', this.api)
+    this.mapController = new MapController((name, id, type) => this.infoContainer.showInfo(name, id, type))
     this.locationSearch = new LocationSearch()
-
 
     this.loadMapData()
     this.searchDebounce = null
@@ -37,7 +38,8 @@ export class ViewController {
     // Download map locations
     const locationTypes = Object.keys(locationLayers)
 
-    this.LayerPanelComponent = new LayerPanelComponent(this.mapController, locationTypes)
+
+    this.layerPanel = new LayerPanelComponent('layer-panel-placeholder', this.mapController, ['kingdom', ...locationTypes])
 
     for (let locationType of locationTypes) {
       const geojson = await this.api.getLocations(locationType)
@@ -50,7 +52,7 @@ export class ViewController {
     const kingdomsGeojson = await this.api.getKingdoms()
     this.locationSearch.addGeoJsonItems(kingdomsGeojson, 'kingdom')
     this.mapController.addKingdomGeojson(kingdomsGeojson)
-    this.toggleMapLayer('kingdom')
+    this.layerPanel.toggleMapLayer('kingdom')
   }
 
   /** Receive search bar input, and debounce by 200 ms */
@@ -88,69 +90,6 @@ export class ViewController {
 
     // Highlight result on map
     this.mapController.selectLocation(searchResult.id, searchResult.layerName)
-  }
-
-  /** Show info when a map item is selected */
-  async showInfo (name, id, type) {
-    const infoTitle = document.getElementById('info-title')
-    infoTitle.innerHTML = `<h1>${name}</h1>`
-
-    const infoContent = document.getElementById('info-content')
-    if (id && type === 'kingdom') {
-      infoContent.innerHTML = this.getKingdomDetailHtml(id)
-    } else {
-      infoContent.innerHTML = this.getLocationDetailHtml(id, type)
-    }
-
-    // Show info window if hidden, and on desktop
-    const infoContainer = document.getElementById('info-container')
-    if (!infoContainer.classList.contains('info-active') && window.innerWidth > 600) {
-      this.toggleInfo()
-    }
-  }
-
-  /** Create kingdom detail HTML string */
-  async getKingdomDetailHtml (id) {
-    let size = await this.api.getRegionSize(id)
-    size = size.toLocaleString(undefined, { maximumFractionDigits: 0 })
-
-    const castleCount = await this.api.getCastleCount(id)
-    const kingdomInfo = await this.api.getRegionDetails(id)
-    const summaryHTML = this.getInfoSummaryHtml(kingdomInfo)
-
-    return `
-      <h3>KINGDOM</h3>
-      <div>Size Estimate - ${size} km<sup>2</sup></div>
-      <div>Number of Castles - ${castleCount}</div>
-      ${summaryHTML}
-      `
-  }
-
-  /** Create location detail HTML string */
-  async getLocationDetailHtml (id, type) {
-    const locationInfo = await this.api.getLocationDetails(id)
-    const summaryHTML = this.getInfoSummaryHtml(locationInfo)
-    return `
-      <h3>${type.toUpperCase()}</h3>
-      ${summaryHTML}
-      `
-  }
-
-  /** Format location summary HTML */
-  getInfoSummaryHtml (info) {
-    return `
-      <h3>Summary</h3>
-      <div>${info.summary}</div>
-      <div>
-        <a href="${info.url}" target="_blank" rel="noopener">Read More...</a>
-      </div>
-    `
-  }
-
-  /** Toggle the info container */
-  toggleInfo () {
-    const infoContainer = document.getElementById('info-container')
-    infoContainer.classList.toggle('info-active')
   }
 }
 
