0285734226ea4b13a0ddbcafcac96de62d7a78b5
triestpa <patrick.triest@gmail.com>
2017-09-07 15:19:15 +0700

    Add more comments to map component

--- app/components/map/map.js
+++ app/components/map/map.js
@@ -5,6 +5,7 @@ import L from 'leaflet'
 export class MapController {
   /** Initialize Map Properties */
   constructor (mapPlaceholderId, props) {
+    // Initialize Leaflet map
     this.map = L.map(mapPlaceholderId, {
       center: [ 5, 20 ],
       zoom: 4,
@@ -13,11 +14,17 @@ export class MapController {
       maxBounds: [ [ 50, -30 ], [ -45, 100 ] ]
     })
 
+    // Position zoom control
     this.map.zoomControl.setPosition('bottomright')
+
+    // Assign location selected callback
     this.onLocationSelected = props.onLocationSelected
-    this.layers = { }
+
+    // Initialize state variables
+    this.layers = {}
     this.selectedRegion = null
 
+    // Render Carto GoT tile baselayer
     L.tileLayer(
       'https://cartocdn-ashbu.global.ssl.fastly.net/ramirocartodb/api/v1/map/named/tpl_756aec63_3adb_48b6_9d14_331c6cbc47cf/all/{z}/{x}/{y}.png',
       { crs: L.CRS.EPSG4326 }).addTo(this.map)
@@ -25,55 +32,56 @@ export class MapController {
 
   /** Add location geojson to the leaflet instance */
   addLocationGeojson (layerTitle, geojson, iconUrl) {
-    const properties = {}
-    const icon = L.icon({ iconUrl, iconSize: [ 24, 56 ] })
-
-    properties.pointToLayer = function (feature, latlng) {
-      return L.marker(latlng, { icon, title: feature.properties.name })
-    }
-
-    properties.onEachFeature = (feature, layer) => {
-      layer.bindPopup(feature.properties.name, { closeButton: false })
-      layer.on({
-        click: async (e) => {
-          this.onLocationSelected(feature.properties.name, feature.properties.id, feature.properties.type)
-          this.setHighlightedRegion(null)
-        }
-      })
-    }
-
-    this.layers[layerTitle] = L.geoJSON(geojson, properties)
+    // Initialize new geojson layer
+    this.layers[layerTitle] = L.geoJSON(geojson, {
+      // Show marker on location
+      pointToLayer: (feature, latlng) => {
+        return L.marker(latlng, {
+          icon: L.icon({ iconUrl, iconSize: [ 24, 56 ] }),
+          title: feature.properties.name })
+      },
+      // Assign on click listener to each location
+      onEachFeature: (feature, layer) => {
+        layer.bindPopup(feature.properties.name, { closeButton: false })
+        layer.on({
+          click: async (e) => {
+            this.setHighlightedRegion(null) // Deselect highlighed region
+            this.onLocationSelected(feature.properties.name, feature.properties.id, feature.properties.type)
+          }
+        })
+      }
+    })
   }
 
   /** Add boundary (kingdom) geojson to the leaflet instance */
   addKingdomGeojson (geojson) {
-    const properties = {
+    // Initialize new geojson layer
+    this.layers.kingdom = L.geoJSON(geojson, {
+      // Set layer style
       style: {
         'color': '#222',
         'weight': 1,
         'opacity': 0.65
+      },
+      // Assign on click listener to each location
+      onEachFeature: (feature, layer) => {
+        layer.on({
+          click: async (e) => {
+            this.onLocationSelected(feature.properties.name, feature.properties.id, 'kingdom')
+            this.map.closePopup() // Deselect selected location marker
+            this.setHighlightedRegion(layer)
+          }
+        })
       }
-    }
-
-    properties.onEachFeature = (feature, layer) => {
-      layer.on({
-        click: async (e) => {
-          this.onLocationSelected(feature.properties.name, feature.properties.id, 'kingdom')
-          this.map.closePopup()
-          this.setHighlightedRegion(layer)
-        }
-      })
-    }
-
-    this.layers.kingdom = L.geoJSON(geojson, properties)
+    })
   }
 
   /** Highlight the selected region */
   setHighlightedRegion (layer) {
-    if (this.selected) {
-      this.layers.kingdom.resetStyle(this.selected)
-    }
+    // If a layer is currently selected, deselect it
+    if (this.selected) { this.layers.kingdom.resetStyle(this.selected) }
 
+    // Select the provided region layer
     this.selected = layer
     if (this.selected) {
       this.selected.bringToFront()
@@ -100,18 +108,21 @@ export class MapController {
 
   /** Trigger "click" on layer with provided name */
   selectLocation (id, layerName) {
+    // Find selected layer
     const geojsonLayer = this.layers[layerName]
     const sublayers = geojsonLayer.getLayers()
     const selectedSublayer = sublayers.find(layer => {
       return layer.feature.geometry.properties.id === id
     })
 
+    // Zoom map to selected layer
     if (selectedSublayer.feature.geometry.type === 'Point') {
       this.map.flyTo(selectedSublayer.getLatLng(), 5)
     } else {
       this.map.flyToBounds(selectedSublayer.getBounds(), 5)
     }
 
+    // Fire click event
     selectedSublayer.fireEvent('click')
   }
 }
