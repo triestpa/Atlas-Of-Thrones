12992bcad112bd145e074bd447af99c78b69b87f
triestpa <patrick.triest@gmail.com>
2017-09-07 15:19:04 +0700

    Add more comments to layer-panel component

--- app/components/layer-panel/layer-panel.js
+++ app/components/layer-panel/layer-panel.js
@@ -5,19 +5,24 @@ import { Component } from '../component'
 export class LayerPanelComponent extends Component {
   constructor (placeholderId, props) {
     super(placeholderId, template)
-    this.onLayerToggle = props.onLayerToggle
-    this.layerPanelElem = this.componentElem.querySelector('[rel=panel]')
 
+    // Assign onLayerToggle callback
+    this.onLayerToggle = props.onLayerToggle
+
+    // Bind UI elements
+    this.layerPanelElem = this.componentElem.querySelector('[rel=panel]')
     this.layerToggleElem = this.componentElem.querySelector('[rel=toggle]')
+    const layerButtons = this.componentElem.querySelector('[rel=buttons]')
+
+    // Toggle layer panel on click (mobile only)
     this.layerToggleElem.addEventListener('click', () => this.toggleLayerPanel())
 
-    const layerButtons = this.componentElem.querySelector('[rel=buttons]')
+    // Add a toggle button for each layer
     for (let layerName of props.layerNames) {
       let layerItem = document.createElement('div')
       layerItem.textContent = `${layerName}s`
-      layerItem.className = 'layer-button'
       layerItem.setAttribute('rel', `${layerName}-toggle`)
-      layerItem.addEventListener('click', () => this.toggleMapLayer(layerName))
+      layerItem.addEventListener('click', (e) => this.toggleMapLayer(layerName))
       layerButtons.appendChild(layerItem)
     }
   }
@@ -29,10 +34,10 @@ export class LayerPanelComponent extends Component {
 
   /** Toggle map layer visibility */
   toggleMapLayer (layerName) {
-    const button = this.componentElem.querySelector(`[rel=${layerName}-toggle]`)
-    button.classList.toggle('toggle-active')
+    // Toggle active UI status
+    this.componentElem.querySelector(`[rel=${layerName}-toggle]`).classList.toggle('toggle-active')
 
+    // Trigger layer toggle callback
     this.onLayerToggle(layerName)
-    // this.mapController.toggleLayer(layerName)
   }
 }
