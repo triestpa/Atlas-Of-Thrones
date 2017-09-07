4198560cda9524d1dcb783911eb9f34a8b714a69
triestpa <patrick.triest@gmail.com>
2017-09-07 23:35:48 +0700

    Add more functionality to base component class

--- app/components/component.js
+++ app/components/component.js
@@ -1,6 +1,28 @@
 export class Component {
-  constructor (placeholderId, template) {
+  constructor (placeholderId, template, events) {
     this.componentElem = document.getElementById(placeholderId)
-    this.componentElem.innerHTML = template
+
+    if (template) {
+      // Load template into placeholder element
+      this.componentElem.innerHTML = template
+
+      // Find all refs in component
+      this.refs = {}
+      const refElems = this.componentElem.querySelectorAll('[ref]')
+      refElems.forEach((elem) => { this.refs[elem.getAttribute('ref')] = elem })
+    }
+
+    if (events) { this.createEvents(events) }
+  }
+
+  createEvents (events) {
+    Object.keys(events).forEach((eventName) => {
+      this.componentElem.addEventListener(eventName, events[eventName], false)
+    })
+  }
+
+  triggerEvent (eventName, detail) {
+    const event = new window.CustomEvent(eventName, { detail })
+    this.componentElem.dispatchEvent(event)
   }
 }
