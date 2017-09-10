018caa5ec7757dd41039ad90cb88957d5e03aebd
triestpa <patrick.triest@gmail.com>
2017-09-10 10:50:15 +0700

    Add webpack config comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const BabiliPlugin = require('babili-webpack-plugin')
 
+// Babel loader for Transpiling ES8 Javascript for browser usage
 const babelLoader = {
   test: /\.js$/,
   loader: 'babel-loader',
@@ -8,26 +9,29 @@ const babelLoader = {
   query: { presets: ['es2017'] }
 }
 
+// SCSS loader for transpiling SCSS files to CSS
 const scssLoader = {
   test: /\.scss$/,
   loader: 'style-loader!css-loader!sass-loader'
 }
 
+// URL loader to resolve data-urls at build time
 const urlLoader = {
   test: /\.(png|woff|woff2|eot|ttf|svg)$/,
   loader: 'url-loader?limit=100000'
 }
 
+// HTML load to allow us to import HTML templated into our JS files
 const htmlLoader = {
   test: /\.html$/,
   loader: 'html-loader'
 }
 
 const webpackConfig = {
-  entry: './app/main.js',
+  entry: './app/main.js', // Start at app/main.js
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js' // Output to public/bundle.js
   },
   module: { loaders: [ babelLoader, scssLoader, urlLoader, htmlLoader ] }
 }
